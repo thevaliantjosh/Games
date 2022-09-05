0d639c6ba89419df7f0647e3475973565d9e8751
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-09-05 14:47:34 -0500

    added data framework

--- js/memory_card_game.js
+++ js/memory_card_game.js
@@ -5,12 +5,28 @@
 
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
+
 //function that will
 function flipCard() {
     // console.log("I was Clicked!");
     // console.log(this);
     //will add or remove the class list of "flip"
     this.classList.toggle("flip");
+    if(!hasFlippedCard) {
+        //first time user has clicked card
+        hasFlippedCard = true;
+        firstCard = this;
+        console.log({hasFlippedCard, firstCard});
+    } else {
+        //second click of Card
+        hasFlippedCard = false;
+        secondCard = this;
+        console.log({hasFlippedCard, secondCard})
+        //do Cards Match?
+    }
 }
 
 //Loop that will loop through the cards list, and attach an event listener, listening for a click even, when fired it executes a function flip card.
