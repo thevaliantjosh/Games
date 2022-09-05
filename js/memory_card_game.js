cb3674d86905e9ee05b6ad8d2100172ee8275a93
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-09-05 15:29:31 -0500

    refactoring, and double-click

--- js/memory_card_game.js
+++ js/memory_card_game.js
@@ -6,11 +6,14 @@
 const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard;
 let secondCard;
 
 //function that will
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
     // console.log("I was Clicked!");
     // console.log(this);
     //will add or remove the class list of "flip"
@@ -23,7 +26,6 @@ function flipCard() {
         return;
     }
         //second click of Card
-        hasFlippedCard = false;
         secondCard = this;
         // console.log({hasFlippedCard, secondCard})
         //do Cards Match?
@@ -40,15 +42,24 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
 }
 
 function unFlipCards() {
+    lockBoard = true;
     //not a match
     setTimeout(()=>{
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
+
+        resetBoard();
     }, 1500);
 }
 
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
 //Loop that will loop through the cards list, and attach an event listener, listening for a click even, when fired it executes a function flip card.
 cards.forEach(card => card.addEventListener("click", flipCard));
